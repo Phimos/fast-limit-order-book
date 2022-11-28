c85bf8c8e5da8ffd30f2b24e515f704f6160eba4
phimos <392959395@qq.com>
2022-11-28 23:52:10 +0800

    <fix> ns unit

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,16 +5,16 @@
 #include <chrono>
 #include <string>
 
-std::string strftime(uint64_t timestamp, const char *format = "%Y-%m-%d %H:%M:%S") // timestamp in us
+std::string strftime(uint64_t timestamp, const char *format = "%Y-%m-%d %H:%M:%S") // timestamp in ns
 {
-    auto tp = std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds>(std::chrono::microseconds(timestamp));
+    auto tp = std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>(std::chrono::nanoseconds(timestamp));
     auto tt = std::chrono::system_clock::to_time_t(tp);
     std::tm gmt{}; // UTC
     gmtime_r(&tt, &gmt);
     char buffer[32];
     strftime(buffer, sizeof(buffer), format, &gmt);
-    std::string micro = std::to_string(timestamp % 1000000);
-    return std::string(buffer) + "." + std::string(6 - micro.size(), '0') + micro;
+    std::string ms = std::to_string((timestamp % 1000000000UL) / 1000000);
+    return std::string(buffer) + "." + std::string(3 - ms.size(), '0') + ms;
 }
 
 #endif // __UTILS_HPP__
