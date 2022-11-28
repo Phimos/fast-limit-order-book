c42adaa6111f7872cd009bb1ccff19887deb117a
phimos <392959395@qq.com>
2022-11-28 20:57:45 +0800

    <feat> add enum type

--- src/flob.cpp
+++ src/flob.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/stl.h>
 
 #include "limit_order_book.hpp"
+#include "struct.hpp"
 
 namespace py = pybind11;
 
@@ -11,5 +12,16 @@ PYBIND11_MODULE(flob, m)
 
     py::class_<LimitOrderBook>(m, "LimitOrderBook")
         .def(py::init<size_t>(), py::arg("decimal_places") = 2)
-        .def("clear", &LimitOrderBook::clear);
+        .def("clear", &LimitOrderBook::clear)
+        .def("show", &LimitOrderBook::show);
+
+    py::enum_<TradingStatus>(m, "TradingStatus")
+        .value("CallAuction", TradingStatus::CallAuction)
+        .value("ContinuousTrading", TradingStatus::ContinuousTrading)
+        .value("ClosingAuction", TradingStatus::ClosingAuction);
+
+    py::enum_<Side>(m, "Side")
+        .value("Bid", Side::Bid)
+        .value("Ask", Side::Ask)
+        .export_values();
 }
