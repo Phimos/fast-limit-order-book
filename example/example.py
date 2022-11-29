07277e8d8d82bd7850e2f81e007ea7df1e10b8a5
phimos <392959395@qq.com>
2022-11-29 17:27:57 +0800

    <script> transaction example

--- example/example.py
+++ example/example.py
@@ -1,13 +1,57 @@
-import flob
+import pandas as pd
 
-book = flob.LimitOrderBook()
-book.load("sample.csv")
-book.set_status("CallAuction")
-book.until(9, 30)
-book.show()
-book.match_call_auction()
-book.show()
+from flob import LimitOrderBook
 
-book.set_status("ContinuousTrading")
-book.until(9, 31)
-book.show()
+PRE_OPENING_SESSION_START = (9, 15)
+PRE_OPENING_SESSION_END = (9, 25)
+
+MORNING_SESSION_START = (9, 30)
+MORNING_SESSION_END = (11, 30)
+
+AFTERNOON_SESSION_START = (13, 0)
+AFTERNOON_SESSION_END = (14, 57)
+
+CLOSING_SESSION_START = (14, 57)
+CLOSING_SESSION_END = (15, 0)
+
+
+lob = LimitOrderBook()
+lob.load("sample.csv")
+
+# Pre-opening session
+lob.set_status("CallAuction")
+lob.until(*PRE_OPENING_SESSION_END)
+lob.match_call_auction()
+lob.show()
+
+# Morning session
+lob.set_status("ContinuousTrading")
+lob.until(*MORNING_SESSION_END)
+lob.show()
+
+# Afternoon session
+lob.until(*AFTERNOON_SESSION_END)
+lob.show()
+
+# Closing session
+lob.set_status("CallAuction")
+lob.until(*CLOSING_SESSION_END)
+lob.show()
+lob.match_call_auction()
+lob.show()
+
+transactions = lob.get_transactions()
+transactions = [
+    {
+        "bid_uid": item.bid_uid,
+        "ask_uid": item.ask_uid,
+        "price": item.price,
+        "volume": item.quantity,
+        "timestamp": item.timestamp,
+    }
+    for item in transactions
+]
+
+transactions = pd.DataFrame(transactions)
+transactions.timestamp = pd.to_datetime(transactions.timestamp)
+print(transactions)
