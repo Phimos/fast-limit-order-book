452a3229c170550f4a31e11285bd8c2bfba1b793
Yunchong Gan <392959395@qq.com>
2022-12-01 14:35:38 +0000

    <script> transactions

--- example/example.py
+++ example/example.py
@@ -3,39 +3,26 @@ import pandas as pd
 import flob
 from flob import LimitOrderBook
 
-PRE_OPENING_SESSION_START = pd.Timedelta("09:15:00")
-PRE_OPENING_SESSION_END = pd.Timedelta("09:25:00")
-
-MORNING_SESSION_START = pd.Timedelta("09:30:00")
-MORNING_SESSION_END = pd.Timedelta("11:30:00")
-
-AFTERNOON_SESSION_START = pd.Timedelta("13:00:00")
-AFTERNOON_SESSION_END = pd.Timedelta("14:57:00")
-
-CLOSING_SESSION_START = pd.Timedelta("14:57:00")
-CLOSING_SESSION_END = pd.Timedelta("15:00:00")
-
-
 schedule = [
     (
         flob.TradingStatus.CallAuction,
-        PRE_OPENING_SESSION_START.value,
-        PRE_OPENING_SESSION_END.value,
+        pd.Timedelta("09:15:00").value,
+        pd.Timedelta("09:25:00").value,
     ),
     (
         flob.TradingStatus.ContinuousTrading,
-        MORNING_SESSION_START.value,
-        MORNING_SESSION_END.value,
+        pd.Timedelta("09:30:00").value,
+        pd.Timedelta("11:30:00").value,
     ),
     (
         flob.TradingStatus.ContinuousTrading,
-        AFTERNOON_SESSION_START.value,
-        AFTERNOON_SESSION_END.value,
+        pd.Timedelta("13:00:00").value,
+        pd.Timedelta("14:57:00").value,
     ),
     (
         flob.TradingStatus.CallAuction,
-        CLOSING_SESSION_START.value,
-        CLOSING_SESSION_END.value,
+        pd.Timedelta("14:57:00").value,
+        pd.Timedelta("15:00:00").value,
     ),
 ]
 
@@ -43,42 +30,20 @@ schedule = [
 lob = LimitOrderBook()
 lob.load("data/sample.csv")
 lob.set_schedule(schedule)
-# lob.run()
+lob.run()
 
-# # Pre-opening session
-# lob.set_status("CallAuction")
-# lob.until(PRE_OPENING_SESSION_END.value)
-# lob.match_call_auction(PRE_OPENING_SESSION_END.value)
-# lob.show()
+transactions = lob.get_transactions()
+transactions = [
+    {
+        "bid_uid": item.bid_uid,
+        "ask_uid": item.ask_uid,
+        "price": item.price,
+        "volume": item.quantity,
+        "timestamp": item.timestamp,
+    }
+    for item in transactions
+]
 
-# # Morning session
-# lob.set_status("ContinuousTrading")
-# lob.until(MORNING_SESSION_END.value)
-# lob.show_transactions()
-
-# # Afternoon session
-# lob.until(AFTERNOON_SESSION_END.value)
-# lob.show()
-
-# # Closing session
-# lob.set_status("CallAuction")
-# lob.until(CLOSING_SESSION_END.value)
-# lob.show()
-# lob.match_call_auction(CLOSING_SESSION_END.value)
-# lob.show()
-
-# transactions = lob.get_transactions()
-# transactions = [
-#     {
-#         "bid_uid": item.bid_uid,
-#         "ask_uid": item.ask_uid,
-#         "price": item.price,
-#         "volume": item.quantity,
-#         "timestamp": item.timestamp,
-#     }
-#     for item in transactions
-# ]
-
-# transactions = pd.DataFrame(transactions)
-# transactions.timestamp = pd.to_datetime(transactions.timestamp)
-# print(transactions)
+transactions = pd.DataFrame(transactions)
+transactions.timestamp = pd.to_datetime(transactions.timestamp)
+print(transactions)
