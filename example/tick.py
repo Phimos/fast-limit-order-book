4401b9d45ba4b1a627d55bf8370a1db8cb380e92
Yunchong Gan <392959395@qq.com>
2022-12-09 14:12:26 +0000

    <script> update tick.py

--- example/tick.py
+++ example/tick.py
@@ -1,56 +1,51 @@
+import argparse
+
 import pandas as pd
 
-import flob
 from flob import LimitOrderBook
 
-schedule = [
-    (
-        flob.TradingStatus.CallAuction,
-        pd.Timedelta("09:15:00").value,
-        pd.Timedelta("09:25:00").value,
-    ),
-    (
-        flob.TradingStatus.ContinuousTrading,
-        pd.Timedelta("09:30:00").value,
-        pd.Timedelta("11:30:00").value,
-    ),
-    (
-        flob.TradingStatus.ContinuousTrading,
-        pd.Timedelta("13:00:00").value,
-        pd.Timedelta("14:57:00").value,
-    ),
-    (
-        flob.TradingStatus.CallAuction,
-        pd.Timedelta("14:57:00").value,
-        pd.Timedelta("15:00:00").value,
-    ),
-]
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", type=str, default="data/sample.csv")
+    parser.add_argument("--schedule", type=str, default="AShare")
+    parser.add_argument("--snapshot_gap", type=str, default="3s")
+    parser.add_argument("--topk", type=int, default=5)
+    parser.add_argument("--output", type=str, default="data/ticks.csv")
+    return parser.parse_args()
 
 
-lob = LimitOrderBook()
-lob.load("data/sample.csv")
-lob.set_schedule(schedule)
-lob.set_snapshot_gap(pd.Timedelta("00:00:03").value)
-lob.run()
+if __name__ == "__main__":
+    args = parse_args()
 
-ticks = lob.get_ticks()
-ticks = [
-    {
-        "timestamp": item.timestamp,
-        "open": item.open,
-        "high": item.high,
-        "low": item.low,
-        "close": item.close,
-        "volume": item.volume,
-        "amount": item.amount,
-        **{f"bid_price_{i+1}": item.bid_prices[i] for i in range(5)},
-        **{f"ask_price_{i+1}": item.ask_prices[i] for i in range(5)},
-        **{f"bid_volume_{i+1}": item.bid_volumes[i] for i in range(5)},
-        **{f"ask_volume_{i+1}": item.ask_volumes[i] for i in range(5)},
-    }
-    for item in ticks
-]
+    lob = LimitOrderBook(
+        schedule=args.schedule,
+        snapshot_gap=pd.Timedelta(args.snapshot_gap).value,
+        topk=args.topk,
+    )
+    lob.load(args.data)
+    lob.run()
 
-ticks = pd.DataFrame(ticks)
-ticks.timestamp = pd.to_datetime(ticks.timestamp)
-print(ticks)
+    ticks = lob.get_ticks()
+    ticks = [
+        {
+            "timestamp": item.timestamp,
+            "open": item.open,
+            "high": item.high,
+            "low": item.low,
+            "close": item.close,
+            "volume": item.volume,
+            "amount": item.amount,
+            **{f"bid_price_{i+1}": item.bid_prices[i] for i in range(args.topk)},
+            **{f"ask_price_{i+1}": item.ask_prices[i] for i in range(args.topk)},
+            **{f"bid_volume_{i+1}": item.bid_volumes[i] for i in range(args.topk)},
+            **{f"ask_volume_{i+1}": item.ask_volumes[i] for i in range(args.topk)},
+        }
+        for item in ticks
+    ]
+    ticks = pd.DataFrame(ticks)
+    ticks.timestamp = pd.to_datetime(ticks.timestamp)
+    ticks = ticks.round({"open": 2, "high": 2, "low": 2, "close": 2, "amount": 2})
+    ticks = ticks.round({f"bid_price_{i+1}": 2 for i in range(args.topk)})
+    ticks = ticks.round({f"ask_price_{i+1}": 2 for i in range(args.topk)})
+    ticks.to_csv(args.output, index=False)
