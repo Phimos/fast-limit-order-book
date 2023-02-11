f0028e766ff92d76eed8c2602f8fb1ca75db152d
Phimos <yunchong@pku.edu.cn>
2023-02-11 17:16:02 +0800

    <build> -O3 as default

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-ext_modules = [Pybind11Extension("flob", ["src/flob.cpp"], include_dirs=["include"])]
+ext_modules = [
+    Pybind11Extension(
+        "flob",
+        ["src/flob.cpp"],
+        include_dirs=["include"],
+        cxx_std=17,
+        extra_compile_args=["-O3"],  # -Ofast may cause precision error
+    )
+]
 
 setup(
     name="flob",
@@ -9,6 +17,7 @@ setup(
     description="fast-limit-order-book",
     author="Yunchong Gan",
     author_email="ganyunchong@gmail.com",
+    url="https://github.com/Phimos/fast-limit-order-book",
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
 )
